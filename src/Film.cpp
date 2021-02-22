a4c6507b8b34b73483bd2ff4c0c5e482593dfc55
markrenan6564 <markrenan6564@users.noreply.github.com>
2021-02-22 11:47:43 +0800

    Update Film.cpp

--- src/Film.cpp
+++ src/Film.cpp
@@ -1,11 +1,64 @@
-#include "Film.h"
+#include "movie.h"
 
-Film::Film()
-{
-    //ctor
+film::film(){
+    StoredYear = 0;
+    StoredPrice = 0.00;
 }
 
-Film::~Film()
-{
-    //dtor
+film::~film(){
+
 }
+
+film::film(string newTitle, string newDirector, int newYear, double newPrice){
+    StoredTitle = newTitle;
+    StoredDirector = newDirector;
+    StoredYear = newYear;
+    StoredPrice = newPrice;
+    StoredStatus = true;
+}
+
+
+
+string film::getTitle() const{
+    return StoredTitle;
+}
+
+string film::getDirector() const{
+    return StoredDirector;
+}
+
+int film::getYear() const{
+    return StoredYear;
+}
+
+double film::getPrice() const{
+    return StoredPrice;
+}
+
+bool film::getStatus() const{
+    return StoredStatus;
+}
+
+
+
+
+void film::setTitle(string newTitle){
+    StoredTitle = newTitle;
+}
+
+void film::setDirector(string newDirector){
+    StoredDirector = newDirector;
+}
+
+void film::setYear(int newYear){
+    StoredYear = newYear;
+}
+
+void film::setPrice(double newPrice){
+    StoredPrice = newPrice;
+}
+
+void film::setStatus(bool newStatus){
+    StoredStatus = newStatus;
+}
+
