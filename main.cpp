df7b37faed67625f59c82a702aa0718deaaac9c2
markrenan6564 <markrenan6564@users.noreply.github.com>
2021-02-22 11:47:02 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,58 @@
 #include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <limits>
+#include <string>
+
+#include "movie.h"
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+static vector<film> MovieStorage;
+
+void mainProgHeader ();
+
+void parentMenu (int&);
+
+void programTerminator (int&);
+
+void mainMenu (int&);
+
+void adminLogin (int&);
+
+void adminMenu (int&);
+
+int main() {
+
+    // Index
+    // Program Terminator = -1
+    // Parent Menu = 0
+    // Main Mode = 1
+    // Admin Login = 2
+    // Admin Mode = 3
+
+
+    int MenuSelector;
+
+    if (MenuSelector == -1) {
+
+    }
+
+    else if () {
+
+    }
+
+    else {
+        parentMenu (MenuSelector);
+    }
+
+
+}
+
+void mainProgHeader () {
+
+    cout << "\n"
+         << " " << "Movie Ticketing System [Version 69.420.666]" << "\n"
+         << " " << "(c) 2021 Group 4 | BSIT 1 - 4. All rights reserved." << "\n"
+         << endl;
 }
