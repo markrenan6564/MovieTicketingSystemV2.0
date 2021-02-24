e38ef608e69426992a90e037d46a035a54abef43
markrenan6564 <markrenan6564@users.noreply.github.com>
2021-02-24 13:16:50 +0800

    Deactivate Snacks Feature Added

--- main.cpp
+++ main.cpp
@@ -43,6 +43,7 @@ static vector<Snack> SavedSnacks;
     void addSnacks (int&, vector<Snack>&);
     void viewSnacks (int&, vector<Snack>&);
     void editSnacks (int&, vector<Snack>&);
+    void deactivateSnacks (int&, vector<Snack>&);
 
 
 int main() {
@@ -151,6 +152,10 @@ int main() {
 
         else if (menuSelector == 323) {
             editSnacks (menuSelector, SavedSnacks);
+        }
+
+        else if (menuSelector == 324 ) {
+            deactivateSnacks (menuSelector, SavedSnacks);
         }
 
         else {
@@ -2714,5 +2719,208 @@ void editSnacks (int& menuSelector, vector<Snack>& savedSnacks) {
         } // while loop for menu selection
 
     } // If there is already instance of film existing
+
+}
+
+void deactivateSnacks (int& menuSelector, vector<Snack>& savedSnacks) {
+
+    if (savedSnacks.empty()) {
+
+        mainProgHeader ();
+
+        cout << " " << "HOME >> ADMIN MODE >> MANAGE SNACKS >> DEACTIVATE SNACKS" << "\n";
+
+        cout << " " << "====================================================" << "\n"
+             << " " << "                 DEACTIVATE SNACKS                  " << "\n"
+             << " " << "====================================================" << "\n"
+             << " " << "[ERR] No snacks to deactivate                        " << "\n"
+             << " " << "====================================================" << "\n"
+             << endl;
+
+        progStop();
+        clrscreen();
+        menuSelector = 32;
+
+    }
+
+    else {
+
+        mainProgHeader ();
+
+        cout << " " << "HOME >> ADMIN MODE >> MANAGE SNACKS >> DEACTIVATE SNACKS" << "\n";
+
+        cout << " " << "====================================================" << "\n"
+             << " " << "                 DEACTIVATE SNACKS                  " << "\n"
+             << " " << "====================================================" << "\n"
+             << " " << "[1] Deactivate Snack                                 " << "\n"
+             << " " << "[0] Manage Snacks                                    " << "\n"
+             << " " << "====================================================" << "\n"
+             << endl;
+
+        while (1) {
+
+            int usersChoice;
+
+            cout << " " << "[INP] Select Option: ";
+            cin >> usersChoice;
+                while (cin.fail()) {
+                    cinError();
+                    inpErr();
+                    cout << " " << "[INP] Select Option: ";
+                    cin >> usersChoice;
+                }
+
+            if (usersChoice ==  1) {
+
+                int numAct = 0;
+
+                for (unsigned int i = 0; i < savedSnacks.size(); i++) {
+
+                    if (savedSnacks[i].getStatus()){
+                        numAct++;
+                    }
+
+                }
+
+                if (numAct == 0) {
+
+                            clrscreen();
+                            mainProgHeader();
+
+                            cout << " " << "HOME >> ADMIN MODE >> MANAGE SNACKS >> DEACTIVATE SNACKS" << "\n";
+
+                            cout << " " << "====================================================" << "\n"
+                                 << " " << "                 DEACTIVATE SNACKS                  " << "\n"
+                                 << " " << "====================================================" << "\n"
+                                 << " " << "[ERR] All snacks are deactivated" << "\n"
+                                 << " " << "====================================================" << "\n"
+                                 << endl;
+
+                            progStop();
+                            clrscreen();
+                            menuSelector = 324;
+                            break;
+
+                }
+
+                else {
+
+                    clrscreen();
+
+                    mainProgHeader ();
+
+                    cout << " " << "HOME >> ADMIN MODE >> MANAGE SNACKS >> DEACTIVATE SNACKS" << "\n";
+
+                    cout << " " << "====================================================" << "\n"
+                         << " " << "                  DEACTIVATE SNACKS                  " << "\n"
+                         << " " << "====================================================" << "\n"
+                         << endl;
+
+                    cout << " " << "----------------------------------------------------" << "\n"
+                         << " " << "[SYS] Showing all active snacks" << "\n"
+                         << " " << "----------------------------------------------------" << "\n"
+                         << endl;
+
+                    int numFilms = savedSnacks.size();
+
+                    for (int realRef = 0; realRef < numFilms; realRef++) {
+
+                        int filmRef = realRef + 1;
+
+                        if (!savedSnacks[realRef].getStatus()) {
+                            continue;
+                        }
+
+                        else {
+
+                            cout << " " << "----------------------------------------------------" << "\n";
+
+                            if (realRef < 9) {
+                                cout << " " << "[0" << filmRef << "] Snack         : " << savedSnacks[realRef].getName() << " - " << savedSnacks[realRef].getSize() << "\n";
+                            }
+                            else {
+                                cout << " " << "[" << filmRef << "]  Snack         : " << savedSnacks[realRef].getName() << " - " << savedSnacks[realRef].getSize() << "\n";
+                            }
+
+                            cout << " " << "----------------------------------------------------" << "\n"
+                                << " " << "     Product Name  : " << savedSnacks[realRef].getName() << "\n"
+                                << " " << "     Product Size  : " << savedSnacks[realRef].getSize() << "\n"
+                                << " " << "     Product Price : " << savedSnacks[realRef].getPrice() << "\n"
+                                << " " << "     Quantity      : " << savedSnacks[realRef].getQuantity() << "\n"
+                                << " " << "----------------------------------------------------" << "\n"
+                                << endl;
+
+                        }
+
+                    }
+
+                    cout << " " << "====================================================" << "\n"
+                         << endl;
+
+                    while (1) {
+
+                        int userChoice;
+
+                        cout << " " << "[INP] Select a Snack for Deactivation: ";
+                        cin >> usersChoice;
+                        while (cin.fail()) {
+                            cinError();
+                            inpErr();
+                            cout << " " << "[INP] Select a Snack for Deactivation: ";
+                            cin >> usersChoice;
+                        }
+
+                        int realRef = usersChoice - 1;
+
+                        if ((usersChoice > 0) && (usersChoice <= savedSnacks.size()) && (savedSnacks[realRef].getStatus())) {
+
+                            cout << " " << "[INF] The Snack \"" << savedSnacks[realRef].getName() << " - " << savedSnacks[realRef].getSize() << "\" has been selected" << "\n"
+                                 << "\n"
+                                 << " " << "====================================================" << "\n"
+                                 << endl;
+
+                            savedSnacks[realRef].setStatus(false);
+
+                            cout << " " << "[SYS] The Snack \"" << savedSnacks[realRef].getName() << " - " << savedSnacks[realRef].getSize() << "\" has been deactivated" << "\n";
+
+                            progStop();
+                            clrscreen();
+                            break;
+
+                        }
+
+                        else {
+
+                            inpErr();
+
+                        }
+
+                    }
+
+                    menuSelector = 324;
+                    break;
+
+                }
+
+            }
+
+            else if (usersChoice == 0) {
+
+                clrscreen();
+                menuSelector = 32;
+                break;
+
+            }
+
+            else {
+
+                inpErr();
+
+            }
+
+        } // while statement
+
+    } // else statement
+
 
 }
